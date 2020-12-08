3b41b9d765985408ded512023a1c289b625517fd
dominika-georgijevna <75664747+dominika-georgijevna@users.noreply.github.com>
2020-12-08 09:59:22 +0100

    Create if_statements.php
    boolean algebra

---
+++ if_statements.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+</head>
+<body>
+
+<?php
+$isMale = true;
+$isTall = true;
+if ($isMale && $isTall){
+    echo "You are a tall male.";
+} else {
+    echo "You are not a male and not tall.";
+}
+?>
+</body>
+</html>
