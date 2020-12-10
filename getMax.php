a0dad97b05a9707460b7a827e3401d389567e896
dominika-georgijevna <75664747+dominika-georgijevna@users.noreply.github.com>
2020-12-10 08:56:03 +0100

    Create getMax.php
    get maximal number function

---
+++ getMax.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+
+</head>
+<body>
+<?php
+function getMax($num1, $num2){
+    if ($num1>$num2){
+        return $num1;
+    }
+    else {
+        return $num2;
+    }
+}
+echo getMax (3, 90);
+?>
+</body>
+</html>
