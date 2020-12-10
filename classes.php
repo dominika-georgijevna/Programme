930cfd4af744f4075b468e547c3d298ab308f976
dominika-georgijevna <75664747+dominika-georgijevna@users.noreply.github.com>
2020-12-10 10:46:58 +0100

    Create classes.php
    Classes introduction

---
+++ classes.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+class Book {
+    var $title;
+    var $author;
+    var $pages;
+}
+
+$book1 = new Book;
+$book1 ->title = "Harry Potter";
+$book1 ->author = "JK Rowling";
+$book1 ->pages = 400;
+
+$book2 = new Book;
+$book2 ->title = "Lord of the Rings";
+$book2 ->author = "JRR Tolkien";
+$book2 ->pages = 700;
+
+echo $book1 -> title;
+?>
+</body>
+</html>
