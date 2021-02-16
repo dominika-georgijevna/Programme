e9503580f4bdd4a745939f6776ab3ca2a62b1935
dominika-georgijevna <75664747+dominika-georgijevna@users.noreply.github.com>
2021-02-16 12:57:27 +0100

    Update Steuerrechner.py

--- Steuerrechner.py
+++ Steuerrechner.py
@@ -1,5 +1,9 @@
 print("Geben Sie Ihr Gehalt in Euro ein:")
 gehalt=int(input())
 steuer=float(0.18)
+if gehalt>2500:
+    steuer=float(0.22)
+else:
+    steuer=float(0.18)
 summe= gehalt * steuer
 print("Es ergibt sich eine Steuer von ", summe, "Euro")
