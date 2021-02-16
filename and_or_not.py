9018f48a93b703bf2f8929ede803301ab5b5e138
dominika-georgijevna <75664747+dominika-georgijevna@users.noreply.github.com>
2021-02-16 13:48:00 +0100

    Create and_or_not.py
    and, or , not

---
+++ and_or_not.py
@@ -0,0 +1,15 @@
+x = 12
+y = 15
+z = 20
+print("x:", x)
+print("y:", y)
+print("z:", z)
+
+if x<y and x<z:
+    print("y ist die klienste Zahl")
+
+if y>x or y>z:
+    print("y ist nicht die kleinste Zahl")
+
+if not y<x:
+    print("y ist nicht kleiner als x")
