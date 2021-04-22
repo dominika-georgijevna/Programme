e805dbf9516f4f54ff035babc0ed5cf77ff6fb45
dominika-georgijevna <75664747+dominika-georgijevna@users.noreply.github.com>
2021-04-22 12:03:25 +0200

    Create rundung.py

---
+++ rundung.py
@@ -0,0 +1,21 @@
+#Positive Zahl
+x = 12/7
+print("x:", x)
+
+rx = round(x,3)
+print("x gerundet auf drei Stellen:", rx)
+rx = round(x)
+print("x gerundet auf null Stellen:", rx)
+ix = int(x)
+print("Das Ergebnis in eine ganze Zahl umgewandelt: ", ix)
+
+#Negative Zahl
+x = -12/7
+print("x:", x)
+
+rx = round(x,3)
+print("x gerundet auf drei Stellen: ", rx)
+rx = round(x)
+print("x gerundet auf null Stellen: ", rx)
+ix = int(x)
+print("Das Ergebnis in eine ganze Zahl umgewandelt: ", ix)
