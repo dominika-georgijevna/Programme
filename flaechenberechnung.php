77c155a954736fb28f4d243b1586051876c5907f
dominika-georgijevna <75664747+dominika-georgijevna@users.noreply.github.com>
2021-11-08 08:26:00 +0100

    Update flaechenberechnung.php

--- flaechenberechnung.php
+++ flaechenberechnung.php
@@ -1,9 +1,16 @@
 <html>  
+<head>
+    <title>Flächenberechnung</title>
+    <h1>Flächenberechnung</h1>
+</head>
 <body>  
 <form method = "post">   
 Länge: <input type="number" name="laenge">   
 <br><br>  
-Breite: <input type="number" name="breite"><br>   
+Breite: <input type="number" name="breite"><br>  
+<br><br>
+Belagart: <input type="name" name="belagart"><br> 
+<br><br>
 <input type = "submit" name = "submit" value="Berechnen">   
 </form>   
 </body>   
@@ -11,9 +18,11 @@ Breite: <input type="number" name="breite"><br>
 <?php   
 if(isset($_POST['submit']))  
     {   
+//Variablen
 $laenge = $_POST['laenge'];   
 $breite = $_POST['breite'];   
-$flaeche = ($laenge*$breite);   
-echo "Die Fläche ist: $flaeche m²";   
+$flaeche = ($laenge*$breite); 
+$belagart = $_POST['belagart'];
+echo "Die Fläche ist: $flaeche m². Die Belagart ist: $belagart .";   
 }   
 ?>  
